a3818abb54355e41d523d46b9bc9f791d27aacf0
Machiedo <dmachi00@fesb.hr>
2020-08-15 19:31:04 +0200

    Added react

---
+++ workoutapp-client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import Navbar from './components/navbar'
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <div className="container">
+          <Switch>
+            <Route exact path="/" component={home}/>
+            <Route exact path="/login" component={login}/> 
+            <Route exact path="/signup" component={signup}/>
+          </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
+
